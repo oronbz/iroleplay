17e83593979a74c163f3c724a83d940c383384a1
Oron Ben Zvi <oronbz@gmail.com>
2020-01-13 15:13:38 +0200

    Initial Commit

---
+++ iRoleplay/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iRoleplay
+//
+//  Created by Oron Ben Zvi on 13/01/2020.
+//  Copyright © 2020 Oron Ben Zvi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
