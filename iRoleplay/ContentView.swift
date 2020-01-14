d89458255f88d448462701627a2400ce8ea99b1f
Oron Ben Zvi <oronbz@gmail.com>
2020-01-14 11:40:05 +0200

    #3 - Navigation Bar Title

--- iRoleplay/ContentView.swift
+++ iRoleplay/ContentView.swift
@@ -16,6 +16,7 @@ struct ContentView: View {
                 Text("Hello World")
                 Text("Hello World")
             }
+            .navigationBarTitle("Professions")
         }
     }
 }
