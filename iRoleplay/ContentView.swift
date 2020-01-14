90df44fa8ef5767cce031cd7861bf894e1d46912
Oron Ben Zvi <oronbz@gmail.com>
2020-01-14 11:35:57 +0200

    #1 - Static list

--- iRoleplay/ContentView.swift
+++ iRoleplay/ContentView.swift
@@ -10,7 +10,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        List {
+            Text("Hello World")
+            Text("Hello World")
+            Text("Hello World")
+        }
     }
 }
 
