ab351444bb530e4a51431ec62f36ea8704d81666
Oron Ben Zvi <oronbz@gmail.com>
2020-01-14 13:15:10 +0200

    #10 - Implement profession row in content view

--- iRoleplay/ContentView.swift
+++ iRoleplay/ContentView.swift
@@ -17,7 +17,7 @@ struct ContentView: View {
                 ForEach(professionBooks) { book in
                     Section(header: Text(book.name)) {
                         ForEach(book.professions) { profession in
-                            Text(profession.name)
+                            ProfessionRow(profession: profession)
                         }
                     }
                 }
