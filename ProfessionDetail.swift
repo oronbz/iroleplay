20158288c0304a0caabba5d3c871e07491d8ac0f
Oron Ben Zvi <oronbz@gmail.com>
2020-01-14 17:44:55 +0200

    Create profession detail

---
+++ ProfessionDetail.swift
@@ -0,0 +1,23 @@
+//
+//  ProfessionDetail.swift
+//  iRoleplay
+//
+//  Created by Oron Ben Zvi on 14/01/2020.
+//  Copyright © 2020 Oron Ben Zvi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfessionDetail: View {
+    let profession: Profession
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfessionDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfessionDetail(profession: Profession.example)
+    }
+}
